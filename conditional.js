1ea8212371a1305e95beac3fbf38f8d7b0bd4f1c
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-01-30 21:37:58 +0530

    First commit

---
+++ conditional.js
@@ -0,0 +1,3 @@
+//The basic commment of javascript This is single line commment
+
+/* This is multiline comments is javascripts */
---
+++ first.js
@@ -0,0 +1,55 @@
+var age = 23;
+var age = 97;
+let a;
+a = 23;
+
+
+
+const PI = 3.14;
+const app = 34;
+
+console.log("Hello world");
+
+console.log("Pankaj is coder = "+ age);
+
+console.log("Namaste Users  "+ app);
+
+console.log("Value of PI is = "+ PI);
+
+console.log(a);
+
+{
+    let PI = 12;
+    console.log("Value of Pi is changing "+ PI);
+
+}
+
+const student = {
+    fullname : "Pankaj badgujar ",
+    age : "23",
+    village : "bhod bk",
+    Collage : "Kbcnmu jalgaon"
+};
+
+console.log(student);
+
+console.log(student.fullname);
+console.log(student.age);
+
+console.log(student["fullname"]);
+
+const profile = {
+     ful_name : "Pankaj badgujar",
+     isfollow : true,
+     message : "yes or no",
+     post : "12",
+     followers : "1k",
+     following : "4",
+     thread : "@pankajbadgujar",
+     AccountType : "enterpreneur",
+     description : "I am student......"
+
+};
+
+console.log(profile);
+console.log(typeof profile["isfollow"]);
