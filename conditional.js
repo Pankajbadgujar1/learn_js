b3ebaf67bef792fb0e07fce2007dac05910bd11b
Pankaj kumar <pankaj14badgujar@gmail.com>
2025-01-30 22:36:18 +0530

    Adding Conditional statement exercise

--- conditional.js
+++ conditional.js
@@ -1,3 +1,44 @@
 //The basic commment of javascript This is single line commment
 
 /* This is multiline comments is javascripts */
+
+//This is program to check wether the person is adult or not .
+
+//Example of conditional statements
+
+
+let age = 38;
+
+if (age >= 18){
+    console.log("You are an adult.");
+
+}else{
+     console.log("You are Minor");
+}
+
+// Arithemetic operations
+
+let a =10;
+let b = 20;
+
+let c;
+a = a+b;
+s = a-b;
+m = a*b;
+d = a/b;
+
+mod = a%b;
+typeof a;
+console.log(typeof a);
+console.log("The addition of a = "+ a + " and b = "+ b +" = ", a);
+console.log("The substraction of a = "+ a + " and b = "+ b +" = ", s);
+console.log("The multiplication of a = "+ a + " and b = "+ b +" = ", m);
+console.log("The division of a = "+ a + " and b = "+ b +" = ", d);
+console.log("The Modulus of a = "+ a + " and b = "+ b +" = " ,a%b);
+
+// The power operator is used to give the power ex 2 ** 3 means 2 * 2 * 2 = 8
+
+// The power operator is used to give the power ex 2 ** 3 means 2 * 2 * 2 * 2 = 16
+console.log("The power operator is " ,2**3);
+
+console.log("The power operator is " ,2**4);
